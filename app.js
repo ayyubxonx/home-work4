3a24429823192f9383b4c0a7f7bd614af9bf2c0b
ayyubxon <ayyubxonxayotxonov77@gmail.com>
2025-07-19 10:13:51 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,102 @@
+1
+
+// let topildi = false;
+
+// while (!topildi) {
+//     let son = Math.floor(Math.random() * 77) + 1;
+//     let javob = prompt(son + " ni o‘yladingmi? (ha/yo‘q)");
+//     if (javob == "ha") {
+//         alert("Topdim!");
+//         topildi = true;
+//     }
+// }
+
+
+2
+
+// let son = +prompt("Son kiriting:");
+// console.log(Math.floor(son));
+// console.log(Math.ceil(son));
+// console.log(Math.round(son));
+
+
+3
+
+// let son = Math.floor(Math.random() * 11);
+// console.log(son);
+
+
+4
+
+
+// let a;
+// let b = null;
+// console.log(a);
+// console.log(b);
+
+
+5
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(Math.floor(Math.random() * 101));
+//   i++;
+// }
+
+
+6
+
+// let s = +prompt("Son kiriting:");
+// console.log(Math.pow(s, 2));
+
+
+7
+
+// let i = 1;
+// while (i <= 20) {
+//   if (i % 2 !== 0) console.log(i);
+//   i++;
+// }
+
+
+8
+
+
+// let t = Math.floor(Math.random() * 10) + 1;
+// let k;
+// do {
+//     k = +prompt("Son kiriting:");
+// } while (k !== t);
+// alert("Topding!");
+
+
+9
+
+
+// let j = 1;
+// while (j < 50) {
+//   if (j === 21) break;
+//   if (j % 3 === 0) console.log(j);
+//   j++;
+// }
+
+
+10
+
+
+// let i = 0;
+// let max;
+// while (i < 5) {
+//   let r = Math.floor(Math.random() * 100) + 1;
+//   console.log(r);
+//   if (i === 0 || r > max) max = r;
+//   i++;
+// }
+// console.log("Eng kattasi:", max);
+
+
+// let son;
+// while (son === undefined || son === null|| isNaN(son)) {
+//     son = +prompt("Son kiriting:");
+// }
+// console.log(son);
